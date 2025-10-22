40700303f8b5109c9662f308a288d8da8cddc3fe
Bernd Müller <github@muellerbernd.de>
2025-10-22 22:55:24 +0200

    [refactor]

--- flake.nix
+++ flake.nix
@@ -6,49 +6,97 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    flake-utils,
-  }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs {inherit system;};
-    in {
-      packages = {
-        dora-cli = pkgs.rustPlatform.buildRustPackage rec {
-          pname = "dora-cli";
-          version = "0.3.6";
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        packages = {
+          dora-cli = pkgs.rustPlatform.buildRustPackage rec {
+            pname = "dora-cli";
+            version = "0.3.6";
 
-          src = pkgs.fetchFromGitHub {
-            owner = "dora-rs";
-            repo = "dora";
-            rev = "v${version}";
-            hash = "sha256-YwEqwA7Eqz7ZJYFfKoPTWkmgsudKpoATcFE6OOwxpbU=";
-          };
+            src = pkgs.fetchFromGitHub {
+              owner = "dora-rs";
+              repo = "dora";
+              rev = "v${version}";
+              hash = "sha256-YwEqwA7Eqz7ZJYFfKoPTWkmgsudKpoATcFE6OOwxpbU=";
+            };
 
-          cargoHash = "sha256-AkungKYGHMK/tUfzh0d88zRRkQfshus7xOHzdtYAT/I=";
-          nativeBuildInputs = [pkgs.pkg-config];
-          buildInputs = [pkgs.openssl];
-          OPENSSL_NO_VENDOR = 1;
-          buildPhase = ''
-            cargo build --release -p dora-cli
-          '';
+            cargoHash = "sha256-vdKA4qfoWEgTOaR1Deve87GiIiKq3uRpuGo66+IflTg=";
+            nativeBuildInputs = [ pkgs.pkg-config ];
+            buildInputs = [ pkgs.openssl ];
+            OPENSSL_NO_VENDOR = 1;
+            buildPhase = ''
+              cargo build --release -p dora-cli
+            '';
 
+            installPhase = ''
+              mkdir -p $out/bin
+              cp target/release/dora $out/bin
+            '';
 
-          installPhase = ''
-            mkdir -p $out/bin
-            cp target/release/dora $out/bin
-          '';
+            doCheck = false;
 
-          doCheck = false;
-
-          meta = {
-            description = "Making robotic applications fast and simple!";
-            homepage = "https://dora-rs.ai/";
-            changelog = "https://github.com/dora-rs/dora/blob/main/Changelog.md";
-            license = pkgs.lib.licenses.asl20;
+            meta = {
+              description = "Making robotic applications fast and simple!";
+              homepage = "https://dora-rs.ai/";
+              changelog = "https://github.com/dora-rs/dora/blob/main/Changelog.md";
+              license = pkgs.lib.licenses.asl20;
+            };
           };
         };
-      };
-    });
+        devShells = {
+          default = pkgs.mkShell {
+            name = "dora-cli";
+            venvDir = "./.venv";
+            buildInputs = [
+              self.packages.${pkgs.system}.dora-cli
+
+              pkgs.rustc
+              pkgs.cargo
+              pkgs.rustPlatform.bindgenHook
+              pkgs.rust-analyzer
+              pkgs.zenoh
+
+              # A Python interpreter including the 'venv' module is required to bootstrap
+              # the environment.
+              pkgs.python3Packages.python
+
+              # This executes some shell code to initialize a venv in $venvDir before
+              # dropping into the shell
+              pkgs.python3Packages.venvShellHook
+
+              pkgs.python3Packages.pyarrow
+              pkgs.python3Packages.uv
+            ];
+
+            # Run this command, only after creating the virtual environment
+            postVenvCreation = ''
+              unset SOURCE_DATE_EPOCH
+              # pip install -r requirements.txt
+
+            '';
+            postShellHook = ''
+              export CARGO_HOME=$(pwd)/.cargo
+              export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${
+                pkgs.lib.makeLibraryPath [
+                  pkgs.libGL
+                  pkgs.wayland
+                  pkgs.libxkbcommon
+                ]
+              };
+            '';
+          };
+        };
+        checks = self.packages;
+      }
+    );
 }
